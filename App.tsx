9b8bfd1b86a3b36ae251d15a53937ed4a3f5c06a
ghdtjgus76 <ghdtjgus76@naver.com>
2024-07-07 23:34:29 +0900

    feat: react native에서 네이티브 모듈 이벤트 처리 로직 추가

--- App.tsx
+++ App.tsx
@@ -1,7 +1,21 @@
 import {Button, SafeAreaView} from 'react-native';
-import CalendarModule from './src/CalendarModule';
+import CalendarModule, {CalendarEventEmitter} from './src/CalendarModule';
+import {useEffect} from 'react';
 
 function App(): React.JSX.Element {
+  useEffect(() => {
+    const subscription = CalendarEventEmitter.addListener(
+      'EventReminder',
+      event => {
+        console.log(event.eventProperty);
+      },
+    );
+
+    () => {
+      subscription.remove();
+    };
+  }, []);
+
   const handlePress = async () => {
     try {
       const eventId = await CalendarModule.createCalendarEvent(
--- src/CalendarModule.ts
+++ src/CalendarModule.ts
@@ -1,9 +1,10 @@
-import {NativeModules} from 'react-native';
+import {NativeEventEmitter, NativeModules} from 'react-native';
 
 interface CalendarModuleInterface {
   createCalendarEvent(name: string, location: string): Promise<number>;
 }
 
 const {CalendarModule} = NativeModules;
+export const CalendarEventEmitter = new NativeEventEmitter(CalendarModule);
 
 export default CalendarModule as CalendarModuleInterface;
