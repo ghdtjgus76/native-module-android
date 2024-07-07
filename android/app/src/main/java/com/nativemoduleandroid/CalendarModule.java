4adb1ba8a5fb5e64063bb288ceba84d17fb11fcc
ghdtjgus76 <ghdtjgus76@naver.com>
2024-07-07 23:34:13 +0900

    feat: CalendarModule js로 이벤트 보내는 로직 추가

--- android/app/src/main/java/com/nativemoduleandroid/CalendarModule.java
+++ android/app/src/main/java/com/nativemoduleandroid/CalendarModule.java
@@ -4,15 +4,22 @@ import android.util.Log;
 
 import androidx.annotation.NonNull;
 
+import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
 
 public class CalendarModule extends ReactContextBaseJavaModule {
+    ReactApplicationContext reactContext;
+    private int listenerCount = 0;
+
     CalendarModule(ReactApplicationContext context) {
         super(context);
+        this.reactContext = context;
     }
 
     @ReactMethod
@@ -24,6 +31,30 @@ public class CalendarModule extends ReactContextBaseJavaModule {
             promise.resolve(eventId);
         } catch (Exception e) {
             promise.reject(e);
+        }
+    }
+
+    private void sendEvent(String eventName, WritableMap params) {
+        this.reactContext
+                .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                .emit(eventName, params);
+    }
+
+    @ReactMethod
+    public void addListener(String eventName) {
+        if (listenerCount == 0) {
+            WritableMap params = Arguments.createMap();
+            params.putString("eventProperty", "eventProperty1");
+            sendEvent(eventName, params);
+        }
+        listenerCount += 1;
+    }
+
+    @ReactMethod
+    public void removeListeners(int count) {
+        listenerCount -= count;
+        if (listenerCount < 0) {
+            listenerCount = 0;
         }
     }
 
