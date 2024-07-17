97ee69857d7d798842a0ce1f83c92ab9c47c0d2c
ghdtjgus76 <ghdtjgus76@naver.com>
2024-07-17 16:37:41 +0900

    feat: ImagePickerModule 등록

--- android/app/src/main/java/com/nativemoduleandroid/MyAppPackage.java
+++ android/app/src/main/java/com/nativemoduleandroid/MyAppPackage.java
@@ -21,6 +21,7 @@ public class MyAppPackage implements ReactPackage {
     public List<NativeModule> createNativeModules(@NonNull ReactApplicationContext reactContext) {
         List<NativeModule> modules = new ArrayList<>();
         modules.add(new CalendarModule(reactContext));
+        modules.add(new ImagePickerModule(reactContext));
 
         return modules;
     }
